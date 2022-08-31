9211672dade19e448d03b33456f52d6e47a37d05
Aaron Gillum <acgillum@gmail.com>
2022-08-31 18:25:50 -0400

    docs: Added comment line for git version testing

--- bikeshare.py
+++ bikeshare.py
@@ -5,7 +5,9 @@ import numpy as np
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
-
+#
+# This is a quick block for git versioning and testing #1
+#
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
@@ -46,27 +48,27 @@ def load_data(city, month, day):
     Returns:
         df - Pandas DataFrame containing city data filtered by month and day
     """
-    
+
     #load the proper csv using the provided key
     df = pd.read_csv(CITY_DATA[city])
-    
+
     #date/time formatting
     df['Start Time'] = pd.to_datetime(df['Start Time'])
-    
+
     #get month and day of week from Start Time
     df['month'] = df['Start Time'].dt.month
     df['day_of_week'] = df['Start Time'].dt.weekday_name
-                     
-    #Filter to month selection                 
+
+    #Filter to month selection
     if month != 'all':
         months = ['january', 'february', 'march', 'april', 'may', 'june']
         month = months.index(month) + 1
         df = df[df['month'] == month]
-                     
+
     #Filter to day selection
     if day != 'all':
-        df = df[df['day_of_week'] == day.title()] 
-                              
+        df = df[df['day_of_week'] == day.title()]
+
     return df
 
 
@@ -82,14 +84,14 @@ def time_stats(df):
      print('Most Common Month: ', month_mode)
     except KeyError:
      print('\nData not available for this dataset.')
-    
+
     #display the most common day of week
     try:
      day_mode = df['day_of_week'].mode()[0]
-     print('Most Common Day of the Week: ', day_mode)                             
+     print('Most Common Day of the Week: ', day_mode)
     except KeyError:
      print('\nMost Common Day of the Week not available for this dataset.')
-    
+
     #display the most common start hour
     df['hour'] = df['Start Time'].dt.hour
     try:
@@ -97,7 +99,7 @@ def time_stats(df):
      print('Most Common Hour of the Day: ', hour_mode)
     except KeyError:
      print('\nMost Common Hour not available for this dataset.')
-    
+
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
@@ -113,23 +115,23 @@ def station_stats(df):
      station_mode = df['Start Station'].mode()[0]
      print('Most Common Start Station: ', station_mode)
     except KeyError:
-     print('\nMost Common Start Station not available for this dataset.')                   
-                   
+     print('\nMost Common Start Station not available for this dataset.')
+
 
     #display most commonly used end station
     try:
      end_station_mode = df['End Station'].mode()[0]
-     print('Most Common End Station: ', end_station_mode)              
+     print('Most Common End Station: ', end_station_mode)
     except KeyError:
      print('\nMost Common End Station not available for this dataset.')
-    
+
     #display most frequent combination of start station and end station trip
     try:
      trip_mode = df.groupby(['Start Station', 'End Station']).size().nlargest(1)
-     print('Most Common Trip:\n', trip_mode.to_string())            
+     print('Most Common Trip:\n', trip_mode.to_string())
     except KeyError:
      print('\nMost Common Trip not available for this dataset.')
-        
+
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
@@ -146,14 +148,14 @@ def trip_duration_stats(df):
      print('Total Travel Time (hours): ', duration_hours)
     except KeyError:
      print('\nTotal Travel Time not available for this dataset.')
-    
+
     #display mean travel time
     try:
      duration_mean = df['Trip Duration'].mean() / 3600.0
      print('Average Travel Time (hours): ', duration_mean)
     except KeyError:
      print('\nAverage Travel Time not available for this dataset.')
-    
+
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
@@ -170,39 +172,39 @@ def user_stats(df):
      print('User Types:\n', user_types.to_string())
     except KeyError:
      print('\nUser Types not available for this dataset.')
-    
+
     #Display counts of gender
     try:
      gender_count = df['Gender'].value_counts()
      print('\nGender Types: \n', gender_count.to_string())
     except KeyError:
      print('\nGender Types not available for this dataset.')
-    
+
     #Display earliest, most recent, and most common year of birth
     try:
      earliest_year = df['Birth Year'].min()
      print('\nEarliest Year: ', earliest_year)
     except KeyError:
      print('\nEarliest Year not available for this dataset.')
-    
+
     try:
      newest_year = df['Birth Year'].max()
      print('Most Recent Year: ', newest_year)
     except KeyError:
      print('\nMost Recent Year not available for this dataset.')
-    
+
     try:
-     year_mode = df['Birth Year'].mode()[0]                    
-     print('Most Common Year: ', year_mode)                   
+     year_mode = df['Birth Year'].mode()[0]
+     print('Most Common Year: ', year_mode)
     except KeyError:
      print('\nMost Common Year not available for this dataset.')
-    
-    
+
+
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
-          
-                   
+
+
 def raw_data(df):
     #display raw data 5 rows at a time
     print('\nRaw Data Sample: ')
@@ -212,10 +214,10 @@ def raw_data(df):
         view_data = input('\nWould you like to see 5 more rows? Enter yes or no.')
         if view_data != 'yes':
             return
-        x = x + 5  
+        x = x + 5
         print(df.iloc[x:x+5])
-              
-              
+
+
 def main():
     while True:
         city, month, day = get_filters()
@@ -226,7 +228,7 @@ def main():
         trip_duration_stats(df)
         user_stats(df)
         raw_data(df)
-        
+
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
